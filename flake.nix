2cef56328ba593ce64f4de3ef9f0e936a18024e1
ZPeppler <peppler.zachary@gmail.com>
2026-04-30 19:52:47 -0400

    Finally have starship setup

--- flake.nix
+++ flake.nix
@@ -16,5 +16,7 @@
     };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+  outputs = inputs: inputs.flake-parts.lib.mkFlake 
+    { inherit inputs; } 
+    (inputs.import-tree ./modules);
 }
